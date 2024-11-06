f8cbe21cd50917394f911a8eaa3caf12f8f97f23
BSD48-419260 <419260@bsd48.org>
2024-11-05 19:03:52 -0800

    Adding Search Function

--- MediaDatabase/mainbase.cpp
+++ MediaDatabase/mainbase.cpp
@@ -7,13 +7,16 @@
 #include "Movie.cpp"
 using namespace std;
 
+//signatures
 void getStringFromInput(char*, int);
 void addMedia(vector<Media*> & MainBase);
 void killMedia(vector<Media*> & MainBase);
 void listMedia(vector<Media*> MainBase);
-void searchMedia(vector<Media*> & MainBase, int index);
+void searchMedia(vector<Media*> MainBase);
 
+//main function
 int main(){
+  //this is the vector I contain all the Media in.
   vector<Media*> MainBase;
   cout<<"==============================="<<endl;
   cout<<"Welcome to:"<<endl;
@@ -43,7 +46,7 @@ int main(){
     }else if(strcmp(input,"PRINT")==0){
       listMedia(MainBase);
     }else if(strcmp(input,"SEARCH")==0){
-
+      searchMedia(MainBase);
     }else if(strcmp(input,"QUIT")==0){
       notQuit=false;
     }else{
@@ -219,17 +222,90 @@ void killMedia(vector<Media*> & MainBase){
 void listMedia(vector<Media*> MainBase){
   for(auto i=MainBase.begin(); i!=MainBase.end(); ++i){
     if((*(*i)).getType()=='G'){
-      cout<<"Type: GAME, Title:"<<static_cast <Game*>(*i)->getTitle()<<", Year:"<<static_cast <Game*>(*i)->getYear()<<", Publisher:"<<static_cast <Game*>(*i)->getPublisher()<<", Rating:"<<static_cast <Game*>(*i)->getRating()<<endl;
+      cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: GAME, Title:"<<static_cast <Game*>(*i)->getTitle()<<", Year:"<<static_cast <Game*>(*i)->getYear()<<", Publisher:"<<static_cast <Game*>(*i)->getPublisher()<<", Rating:"<<static_cast <Game*>(*i)->getRating()<<endl;
     }else if((*(*i)).getType()=='U'){
-      cout<<"Type: MUSIC, Title:"<<static_cast <Music*>(*i)->getTitle()<<", Artist:"<<static_cast <Music*>(*i)->getArtist()<<", Year:"<<static_cast <Music*>(*i)->getYear()<<", Duration:"<<static_cast <Music*>(*i)->getDuration()<<", Publisher:"<<static_cast <Music*>(*i)->getPublisher()<<endl;
+      cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MUSIC, Title:"<<static_cast <Music*>(*i)->getTitle()<<", Artist:"<<static_cast <Music*>(*i)->getArtist()<<", Year:"<<static_cast <Music*>(*i)->getYear()<<", Duration:"<<static_cast <Music*>(*i)->getDuration()<<", Publisher:"<<static_cast <Music*>(*i)->getPublisher()<<endl;
     }else if((*(*i)).getType()=='O'){
-      cout<<"Type: MOVIE, Title:"<<static_cast <Movie*>(*i)->getTitle()<<", Director:"<<static_cast <Movie*>(*i)->getDirector()<<", Year:"<<static_cast <Movie*>(*i)->getYear()<<", Duration:"<<static_cast <Movie*>(*i)->getDuration()<<", Rating:"<<static_cast <Movie*>(*i)->getRating()<<endl;
+      cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MOVIE, Title:"<<static_cast <Movie*>(*i)->getTitle()<<", Director:"<<static_cast <Movie*>(*i)->getDirector()<<", Year:"<<static_cast <Movie*>(*i)->getYear()<<", Duration:"<<static_cast <Movie*>(*i)->getDuration()<<", Rating:"<<static_cast <Movie*>(*i)->getRating()<<endl;
     }else{
       cout<<"ERROR! UNTYPED MEDIA"<<endl;
     }
   }
   return;
 }
-void searchMedia(vector<Media*> & MainBase, int index){
 
+void searchMedia(vector<Media*> MainBase){
+  bool notQuit=true;
+  char*input = new char[16];
+  int searchIndex;
+  while(notQuit){
+    cout<<"What field do you wish to search by? (TITLE, YEAR, CANCEL)"<<endl;
+    getStringFromInput(input, 16);
+    cout<<"Function got: "<<endl;
+    for(int i=0; i<16; i++){
+      cout<<input[i];
+    }
+    cout<<endl;
+    notQuit=false;
+    if(strcmp(input,"TITLE")==0){
+      searchIndex=1;
+    }else if(strcmp(input,"YEAR")==0){
+      searchIndex=2;
+    }else if(strcmp(input,"CANCEL")==0){
+      return;
+    }else{
+      cout<<"Invalid Command."<<endl;
+      notQuit=true;
+    }
+  }
+  
+  int numberfound=0;
+  if(searchIndex==1){
+    cout<<"Search by title name:"<<endl;
+    getStringFromInput(input, 16);
+    for(auto i=MainBase.begin(); i!=MainBase.end(); ++i){
+      if (strcmp((input),(*i)->getTitle())==0){
+        numberfound++;
+        if((*(*i)).getType()=='G'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: GAME, Title:"<<static_cast <Game*>(*i)->getTitle()<<", Year:"<<static_cast <Game*>(*i)->getYear()<<", Publisher:"<<static_cast <Game*>(*i)->getPublisher()<<", Rating:"<<static_cast <Game*>(*i)->getRating()<<endl;
+        }else if((*(*i)).getType()=='U'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MUSIC, Title:"<<static_cast <Music*>(*i)->getTitle()<<", Artist:"<<static_cast <Music*>(*i)->getArtist()<<", Year:"<<static_cast <Music*>(*i)->getYear()<<", Duration:"<<static_cast <Music*>(*i)->getDuration()<<", Publisher:"<<static_cast <Music*>(*i)->getPublisher()<<endl;
+        }else if((*(*i)).getType()=='O'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MOVIE, Title:"<<static_cast <Movie*>(*i)->getTitle()<<", Director:"<<static_cast <Movie*>(*i)->getDirector()<<", Year:"<<static_cast <Movie*>(*i)->getYear()<<", Duration:"<<static_cast <Movie*>(*i)->getDuration()<<", Rating:"<<static_cast <Movie*>(*i)->getRating()<<endl;
+        }else{
+          cout<<"ERROR! UNTYPED MEDIA"<<endl;
+        }
+      }
+    }
+  }else if(searchIndex==2){
+    bool sacin=false;
+    int nwyear;
+    while(sacin==false){
+      cin>>nwyear;
+      if(cin.fail()){
+        cout<<"I think you did something wrong. Please try again."<<endl;
+        cin.clear();
+        cin.ignore(100000,'\n');
+      }else{
+        sacin=true;
+      }
+    }
+    cin.ignore(100000,'\n');
+    
+    for(auto i=MainBase.begin(); i!=MainBase.end(); ++i){
+      if (nwyear==((*i)->getYear())){
+        numberfound++;
+        if((*(*i)).getType()=='G'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: GAME, Title:"<<static_cast <Game*>(*i)->getTitle()<<", Year:"<<static_cast <Game*>(*i)->getYear()<<", Publisher:"<<static_cast <Game*>(*i)->getPublisher()<<", Rating:"<<static_cast <Game*>(*i)->getRating()<<endl;
+        }else if((*(*i)).getType()=='U'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MUSIC, Title:"<<static_cast <Music*>(*i)->getTitle()<<", Artist:"<<static_cast <Music*>(*i)->getArtist()<<", Year:"<<static_cast <Music*>(*i)->getYear()<<", Duration:"<<static_cast <Music*>(*i)->getDuration()<<", Publisher:"<<static_cast <Music*>(*i)->getPublisher()<<endl;
+        }else if((*(*i)).getType()=='O'){
+          cout<<"Index: "<<static_cast<int>(i-MainBase.begin())<<", Type: MOVIE, Title:"<<static_cast <Movie*>(*i)->getTitle()<<", Director:"<<static_cast <Movie*>(*i)->getDirector()<<", Year:"<<static_cast <Movie*>(*i)->getYear()<<", Duration:"<<static_cast <Movie*>(*i)->getDuration()<<", Rating:"<<static_cast <Movie*>(*i)->getRating()<<endl;
+        }else{
+          cout<<"ERROR! UNTYPED MEDIA"<<endl;
+        }
+      }
+    }
+  }
+  cout<<"Total media found; "<<numberfound<<endl;
 }
